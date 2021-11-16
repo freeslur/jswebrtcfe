1d50444461df23f0f88fa97a2b3ae2363a93d342
Namyong Kim <freeslur@gmail.com>
2021-11-16 17:44:28 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var _socket = _interopRequireDefault(require("socket.io"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+const socket = (0, _socket.default)();
 const myFace = document.getElementById("myFace");
 let myStream;
 
