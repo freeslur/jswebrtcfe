d226948732b9ef4d05c6be97480fedfc7ce43b4b
Namyong Kim <freeslur@gmail.com>
2021-11-16 17:23:18 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-"use strict";
+import "regenerator-runtime/runtime.js";
+import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.promise.js";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
-
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 var myFace = document.getElementById("myFace");
 var myStream;
@@ -14,8 +14,8 @@ function getMedia() {
 }
 
 function _getMedia() {
-  _getMedia = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee() {
-    return _regenerator["default"].wrap(function _callee$(_context) {
+  _getMedia = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
