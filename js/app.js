f48f0538f74e1c2b2ca12130aa9afbe4389fce7e
Namyong Kim <freeslur@gmail.com>
2021-11-16 16:43:25 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -1,40 +1,18 @@
 "use strict";
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
 var myFace = document.getElementById("myFace");
 var myStream;
 
 function getMedia() {
-  return _getMedia.apply(this, arguments);
-}
-
-function _getMedia() {
-  _getMedia = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            try {
-              // myStream = await navigator.mediaDevices.getUserMedia({
-              //   audio: true,
-              //   video: true,
-              // });
-              console.log(myStream);
-            } catch (e) {
-              console.log(e.message);
-            }
-
-          case 1:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  }));
-  return _getMedia.apply(this, arguments);
+  try {
+    // myStream = await navigator.mediaDevices.getUserMedia({
+    //   audio: true,
+    //   video: true,
+    // });
+    console.log(myStream);
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 getMedia();
