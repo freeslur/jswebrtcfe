b09c6cfbfe31c784d36a77bb0cbc2ffba110e7f1
Namyong Kim <freeslur@gmail.com>
2021-11-16 17:17:53 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-import _asyncToGenerator from "@babel/runtime/helpers/asyncToGenerator";
-import _regeneratorRuntime from "@babel/runtime/regenerator";
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
 var myFace = document.getElementById("myFace");
 var myStream;
 
@@ -8,8 +14,8 @@ function getMedia() {
 }
 
 function _getMedia() {
-  _getMedia = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
-    return _regeneratorRuntime.wrap(function _callee$(_context) {
+  _getMedia = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee() {
+    return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
