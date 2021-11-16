be634745fa8c964d206b963a2009885772b220a2
Namyong Kim <freeslur@gmail.com>
2021-11-16 17:27:47 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -1,49 +1,19 @@
 "use strict";
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+const myFace = document.getElementById("myFace");
+let myStream;
 
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+async function getMedia() {
+  try {
+    myStream = await myFace.style.width; // myStream = await navigator.mediaDevices.getUserMedia({
+    //   audio: true,
+    //   video: true,
+    // });
 
-var myFace = document.getElementById("myFace");
-var myStream;
-
-function getMedia() {
-  return _getMedia.apply(this, arguments);
-}
-
-function _getMedia() {
-  _getMedia = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.prev = 0;
-            _context.next = 3;
-            return myFace.style.width;
-
-          case 3:
-            myStream = _context.sent;
-            // myStream = await navigator.mediaDevices.getUserMedia({
-            //   audio: true,
-            //   video: true,
-            // });
-            console.log(myStream);
-            _context.next = 10;
-            break;
-
-          case 7:
-            _context.prev = 7;
-            _context.t0 = _context["catch"](0);
-            console.log(_context.t0.message);
-
-          case 10:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee, null, [[0, 7]]);
-  }));
-  return _getMedia.apply(this, arguments);
+    console.log(myStream);
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 getMedia();
